ae9b9d10a98f4cd1b16ad20e1e268b72cceddb86
peter <85779012+ptah99@users.noreply.github.com>
2021-12-10 13:04:30 +0300

    Create matplotl.py

---
+++ matplotl.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 3*np.pi, 0.1)
+y = np.tan(x)
+plt.plot(x, y)
+# plt.show()
+
+ar = np.array([1, 2, 3])
+print(np.exp(ar))
+print(np.log(ar))  # log= natural log(log base e)
+print(np.log10(ar))
