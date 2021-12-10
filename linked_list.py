079ae750d0dd2bd2bde2059d25f963c1f8f2d9a9
peter <85779012+ptah99@users.noreply.github.com>
2021-12-10 13:01:21 +0300

    linked list

---
+++ linked_list.py
@@ -0,0 +1,138 @@
+class Node:
+    """
+        An object for storing a single node of a linked list.
+        models two attributes - data and link to the next node in the list
+    """
+
+    data = None
+    next_node = None
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return "<Node data: %s>" % self.data
+
+
+class LinkedList:
+    """
+    singly linked list
+    """
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        """
+        Returns the number of nodes in the list
+         Takes O(n) time
+        """
+        current = self.head
+        count = 0
+
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
+
+    def add(self, data):
+        """
+        Adds a new node containing data at head of the list
+        Takes O(1) time
+        """
+        new_node = Node(data)
+        new_node.next_node = self.head
+        self.head = new_node
+
+    def search(self, key):
+        """
+        Search for the first node containing data that matches the key
+         Return the node or none if not found
+
+         Takes O(n) time
+        """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            else:
+                current = current.next_node
+        return None
+
+    def insert(self, data, index):
+        """
+        Inserts a new node containing data at index position
+        Insertion takes O(1) time but finding the node at the
+        insertion point takes O(n) time.
+
+        Takes overall O(n) time
+        """
+
+        if index == 0:
+            self.add(data)
+        if index > 0:
+            new = Node(data)
+
+            position = index
+            current = self.head
+
+            while position > 1:
+                current = node.next_node
+                position -= 1
+
+            prev_node = current
+            next_node = current.next_node
+
+            prev_node.next_node = new
+            new.next_node = next_node
+
+    def remove(self, key):
+        """
+        Removes Node containing data that matches the key
+        Returns the node or None if the key doesnt exist
+        Takes O(n) time
+        """
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                previous.next_node = current.next_node
+
+        return current
+
+    def __repr__(self):
+        """
+        REturn a string representation of the list
+        Take 0(n)
+        """
+        nodes = []
+        current = self.head
+        while current:
+            if current is self.head:
+                nodes.append("[Head:%s]" % current.data)
+            elif current.next_node is None:
+                nodes.append("[Tail: %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+
+            current = current.next_node
+        return '->'.join(nodes)
+
+
+l = LinkedList()
+l.add(1)
+l.add(10)
+l.add(3)
+l.add(4)
+n = l.search(10)
+print(n)
+print(l)
